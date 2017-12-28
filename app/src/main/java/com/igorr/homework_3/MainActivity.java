e0a4075263506f34a7e18a66de4ae5f306d9c0b6
Igorr <portnowIgor86@yandex.ru>
2017-12-28 22:21:24 +0300

    no message

--- app/src/main/java/com/igorr/homework_3/MainActivity.java
+++ app/src/main/java/com/igorr/homework_3/MainActivity.java
@@ -2,26 +2,74 @@ package com.igorr.homework_3;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ActionMenuView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnContinue;
+    Button btnRegistration;
+    Button btnVk;
+    Button btnFacebook;
+    Button btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnContinue = findViewById(R.id.btn_continue);
+        btnRegistration = findViewById(R.id.btn_registration);
+        btnVk = findViewById(R.id.btn_vk);
+        btnFacebook = findViewById(R.id.btn_facebook);
+        btnGoogle = findViewById(R.id.btn_google);
 
+        final Intent smsConfirm = new Intent(this, confirmActivity.class);
+
+        OnClickListener onClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_continue: {
+                        startActivity(smsConfirm);
+                        break;
+                    }
+                    case R.id.btn_vk: {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com")));
+                        break;
+                    }
+                    case R.id.btn_facebook: {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://facebook.com")));
+                        break;
+                    }
+                    case R.id.btn_google: {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com")));
+                        break;
+                    }
+                    case R.id.btn_registration: {
+
+                        break;
+                    }
+                }
+            }
+        };
+        ActionMenuView actM = new ActionMenuView(this);
+        btnContinue.setOnClickListener(onClickListener);
+        btnRegistration.setOnClickListener(onClickListener);
+        btnVk.setOnClickListener(onClickListener);
+        btnFacebook.setOnClickListener(onClickListener);
+        btnGoogle.setOnClickListener(onClickListener);
     }
     @Override
-    protected void onStart(){
-        super.onStart();
-            //Явный интент
-            Intent intent = new Intent(this, confirmActivity.class);
-          //  startActivity(intent);
-
-            Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com"));
-//          startActivity(intent2);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("aaa");
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/igorr/homework_3/confirmActivity.java
+++ app/src/main/java/com/igorr/homework_3/confirmActivity.java
@@ -1,19 +1,27 @@
 package com.igorr.homework_3;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by Igorr on 25.12.2017.
  */
 
 public class confirmActivity extends AppCompatActivity {
+    CoordinatorLayout coordinatorLayout;
+    TextView textView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+   // @Override
+    protected void onCreate (Bundle savedInstanceState) throws NullPointerException {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.confirm_activity);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        textView = findViewById(R.id.textView);
 
     }
 }
